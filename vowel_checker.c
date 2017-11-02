bcec0bb70b94327721d8093989dc4affa78b96f1
AnuragVachaswa <33130064+AnuragVachaswa@users.noreply.github.com>
2017-11-02 19:15:21 +0530

    Create vowel_checker.c

---
+++ vowel_checker.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if(c=='A' ||c=='A'||c=='u' ||c=='U' ||c=='o' ||c=='O' ||c=='I' ||c=='i' ||c=='e' ||c=='E' ||c=='a')
+printf("vowel");
+else
+printf("consonant");
+
+}
